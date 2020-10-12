8c1ec6e24eca58cf75ae7005dc1593ce7b6866f1
VIVEK GUPTA <vivek.jmdsmvdu@gmail.com>
2020-10-12 17:56:44 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoanComponent } from './loan/loan.component';
+import { AuthGuard } from './auth.guard';
+const routes: Routes = [
+    {path: 'login' , component: LoginComponent},
+    {path: 'signup' , component: SignupComponent},
+    {path: 'loan' , canActivate: [AuthGuard], component: LoanComponent},
+    {path: '' , redirectTo: 'login', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
